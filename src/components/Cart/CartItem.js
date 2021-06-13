6b23a8c112433a037a6efe29f60ff330a35cac0f
Grigoriy <xib80@yandex.ru>
2021-06-13 04:00:40 +0300

    fix css of buttons

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -14,7 +14,7 @@ const CartItem = props => {
         </div>
       </div>
       <div className={cls.actions}>
-        <button onClick={props.onRemove} className={cls.minus}>−</button>
+        <button onClick={props.onRemove}>−</button>
         <button onClick={props.onAdd}>+</button>
       </div>
     </li>
